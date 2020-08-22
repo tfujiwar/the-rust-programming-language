b05e8f8e192dd1d403bd06ee92ba4d0ae0484d1b
Taketoshi Fujiwara <taketoshi.fujiwara@gmail.com>
2020-08-22 21:36:34 +0900

    3.2 data types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -15,4 +15,18 @@ fn main() {
     let spaces = " ";
     let spaces = spaces.len();
     println!("spaces = {}", spaces);
+
+    let _hoge: i32 = "-123".parse().expect("not a number");
+
+    let byte: u8 = b'A';
+    let ch: char = 'A';
+    println!("{}, {}", byte, ch);
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (a, b, c) = tup;
+    println!("{}, {}, {}", a, b, c);
+    println!("{}, {}, {}", tup.0, tup.1, tup.2);
+
+    let arr = [1, 2, 3];
+    println!("{}, {}, {}", arr[0], arr[1], arr[2]);
 }

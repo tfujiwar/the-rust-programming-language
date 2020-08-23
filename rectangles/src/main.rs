cb2c0da1f6eac3058fd5da865513a417a8e4c3f2
Taketoshi Fujiwara <taketoshi.fujiwara@gmail.com>
2020-08-23 14:50:11 +0900

    5.3 method syntax

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -3,7 +3,17 @@ fn main() {
             width: 3,
             height: 4,
     };
-    println!("{:?}: {}", r, area(&r));
+    println!("{:?}: {}", r, r.area());
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("{}", rect1.can_hold(&rect2));
+    println!("{}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(10);
+    println!("{:?}", sq);
 }
 
 #[derive(Debug)]
@@ -12,6 +22,16 @@ struct Rectangle {
     height: u32,
 }
 
-fn area(r: &Rectangle) -> u32 {
-    r.width * r.height
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
 }

270b9aa67c3f388c5ae3718e52ba2bcb230f84bd
Taketoshi Fujiwara <taketoshi.fujiwara@gmail.com>
2020-08-29 21:42:18 +0900

    15.4 rc

---
+++ rc/src/main.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+use std::rc::Rc;
+
+fn main() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("{}", Rc::strong_count(&a));
+
+    let b = Cons(3, Rc::clone(&a));
+    println!("{}", Rc::strong_count(&a));
+
+    let c = Cons(4, Rc::clone(&a));
+    println!("{}", Rc::strong_count(&a));
+
+    let c = Cons(4, Rc::clone(&a));
+    println!("{}", Rc::strong_count(&a));
+
+    println!("{:?}", a);
+    println!("{:?}", b);
+    println!("{:?}", c);
+}
